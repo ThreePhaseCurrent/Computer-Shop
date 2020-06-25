8fd21ecc7bc5343a73ec8c7f3f47c2bc1bc7d735
unknown <drakula00776@gmail.com>
2020-06-26 01:13:23 +0300

    Initial commit

---
+++ src/ComputerShop.API/Controllers/AuthControllers.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ComputerShop.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthControllers : Controller
+    {
+        public IActionResult Login()
+        {
+            return Unauthorized();
+        }
+    }
+}
